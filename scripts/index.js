1141ad3fc170e1fc12a6293437090fa774e8e042
delioncourts <delioncourtles@yandex.ru>
2022-01-02 15:58:23 +0300

    upd styles (prettier), html and blocks

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,44 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
-const popup = document.querySelector ('.popup');
+const popup = document.querySelector('.popup');
 
-function openPopup () {
+function openPopup() {
     popup.classList.add('popup_opened');
 }
 
-function closePopup () {
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_name');
+console.log(nameInput);
+// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_job');// Воспользуйтесь инструментом .querySelector()
+console.log(jobInput);
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    document.getElementById('name').value
+    document.getElementById('description').value
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    nameInput.textContent = nameInput.value;
+    jobInput.textContent = jobInput.value;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
