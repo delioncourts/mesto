cc780237872039ddc3d6c949a688f32a48553ce1
delioncourts <delioncourtles@yandex.ru>
2022-01-07 18:19:57 +0300

    upd javascript (form)

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,33 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value=profileTitle.textContent;
+    jobInput.value=profileSubtitle.textContent;
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    // Получите значение полей jobInput и nameInput из свойства value
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent=nameInput.value;
+    profileSubtitle.textContent=jobInput.value;
+    closePopup();
+}
+
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_name');
-
-// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__input_job');// Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-    nameInput.textContent = nameInput.value;
-    jobInput.textContent = jobInput.value;
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
