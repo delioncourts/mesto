66aa56f259f18b7440038e70fc009828cd8d4c3c
delioncourts <delioncourtles@yandex.ru>
2022-01-27 00:10:17 +0300

    upd js (like)

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,8 @@ const initialCards = [
   ];
 
 
-  //Профиль
+  //Профиль 
+
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const profileTitle = document.querySelector('.profile__title');
@@ -35,7 +36,6 @@ const profileSubtitle = document.querySelector('.profile__subtitle');
   //Модалки
 const editModal = document.querySelector('.popup_type_edit');
 const addCardModal = document.querySelector('.popup_type_add-card');
-const imageCardModal = document.querySelector('.popup_type_open-card');
 
 //формы
 const editForm = editModal.querySelector('.popup__form');
@@ -48,9 +48,6 @@ const closeModalEditButton = editModal.querySelector('.popup__close');
 const addCardButton = document.querySelector('.profile__add-button');
 const closeAddCardButton = addCardModal.querySelector('.popup__close');
 
-const openImageCard = document.querySelector('.popup__container-open');
-const closeImageCard = imageCardModal.querySelector('.popup__close');
-
 // инпуты 
 const inputCardName = document.querySelector('.popup__input_type_card-name');
 const inputCardLink = document.querySelector('.popup__input_type_card-link');
@@ -58,25 +55,22 @@ const inputCardLink = document.querySelector('.popup__input_type_card-link');
 const inputProfileName = document.querySelector('.popup__input_type_name');
 const inputProfileDescription = document.querySelector('.popup__input_type_job');
 
-// для карточек
+// ADD CARD
 const list = document.querySelector('.cards__grid');
 const cardTemplate = document.querySelector('.card-template').content;
-const popupOpenSubtitle = document.querySelector('.popup__open-photo-subtitle');
-const popupOpenPhoto = document.querySelector('.popup__open-photo');
 
 // открытие и закрытие попапов
 function toggleModal(modal) {
     modal.classList.toggle('popup_opened');
 }
 
-editProfileButton.addEventListener('click', () => toggleModal(editModal));
-closeModalEditButton.addEventListener('click', () => toggleModal(editModal));
+editProfileButton.addEventListener('click', () => toggleModal(editModal))
+closeModalEditButton.addEventListener('click', () => toggleModal(editModal))
 
-addCardButton.addEventListener('click', () => toggleModal(addCardModal));
-closeAddCardButton.addEventListener('click', () => toggleModal(addCardModal));
 
-openImageCard.addEventListener('click', () => toggleModal(imageCardModal));
-closeImageCard.addEventListener('click', () => toggleModal(imageCardModal));
+addCardButton.addEventListener('click', () => toggleModal(addCardModal))
+closeAddCardButton.addEventListener('click', () => toggleModal(addCardModal))
+
 
 // добавить карточку 
 addCardForm.addEventListener('submit', (event) => {
@@ -90,28 +84,16 @@ createCard({
 toggleModal(addCardModal)
 })
 
-// изменить название профиля
 editForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     profileTitle.textContent=nameInput.value;
     profileSubtitle.textContent=jobInput.value;
     
+
 toggleModal(editModal)
 })
 
-// удаление карточки
-function deleteHandler(e) {
-    e.target.closest('.card').remove()
-    cardElement.remove();
-}
-
-// поставить лайк
-function likeHandler(e) {
-    e.target.closest('.card__like').classList.toggle('card__like-active');
-}
-
-// создать карточку
 function createCard(cardData) {
         const cardElement = cardTemplate.cloneNode(true);
         const cardImage = cardElement.querySelector('.card__image');
@@ -123,20 +105,15 @@ function createCard(cardData) {
         cardImage.src = cardData.link;
         cardImage.alt = cardTitle.textContent;
 
-        deleteButton.addEventListener('click', deleteHandler);
+        deleteButton.addEventListener('click', () => {
+         cardElement.remove();
+         })
 
-        likeButton.addEventListener('click', likeHandler);
-
-        cardImage.addEventListener('click', function() {
-            popupOpenSubtitle.textContent = cardData.name;
-            popupOpenPhoto.src = cardData.link;
-            popupOpenPhoto.alt = popupOpenSubtitle.textContent;
-
-            toggleModal(imageCardModal)
-        })
+        likeButton.addEventListener('click', () => {
+         likeButton.classList.toggle('card__like-active')
+         })
 
         list.prepend(cardElement);
     }
 
-    //добавить карточки из массива
     initialCards.forEach(createCard)
